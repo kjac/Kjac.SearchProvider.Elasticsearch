e014cd9c6f694a80f9b382a4e9277cc080357c08
kjac <kja@umbraco.dk>
2025-07-08 19:32:48 +0200

    Add test site

--- src/Kjac.SearchProvider.Elasticsearch.Site/DependencyInjection/SiteComposer.cs
+++ src/Kjac.SearchProvider.Elasticsearch.Site/DependencyInjection/SiteComposer.cs
@@ -1,10 +1,22 @@
 ﻿using Kjac.SearchProvider.Elasticsearch.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Search.Core.DependencyInjection;
 
 namespace Kjac.SearchProvider.Elasticsearch.Site.DependencyInjection;
 
 public class SiteComposer : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
-        => builder.AddElasticsearchSearchProvider();
+    {
+        builder
+            // add core services for search abstractions
+            .AddSearchCore()
+            // use the Elasticsearch search provider
+            .AddElasticsearchSearchProvider()
+            // force rebuild indexes after startup
+            .RebuildIndexes();
+
+        // configure System.Text.Json to allow serializing output models
+        builder.ConfigureJsonOptions();
+    }
 }
